f1657a0dc0b28b379d9c1844ec67144e5abc1e90
Kushal Neupane <mkushal10@gmail.com>
2020-07-27 14:21:35 +0545

    Primary Layout Designs

--- app/src/main/java/com/example/videomeeting/SignUpActivity.java
+++ app/src/main/java/com/example/videomeeting/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.example.videomeeting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -12,7 +14,7 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         findViewById(R.id.imageBack).setOnClickListener(v -> onBackPressed());
-        findViewById(R.id.textSignIn).setOnClickListener(v -> onBackPressed());
+        findViewById(R.id.textSignIn).setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), SignInActivity.class)));
 
     }
 }
